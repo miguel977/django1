140e7a051f064851a3fd1ae3dc28abca5fe10460
miguel977 <miguel645@gmail.com>
2025-01-30 08:05:05 -0300

    criando templates no django

--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'curso': 'Programação web com django framework',
+        'outro': 'Django é massa'
+    }
+    return render(request, 'index.html', context)
 
 def contato(request):
-    return render(request, ' contato.html')
+    return render(request, 'contato.html')
