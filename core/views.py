d1722f3f20672cedae7a97c6880c4aaf4e20d0e6
miguel977 <miguel645@gmail.com>
2025-02-05 07:47:35 -0300

    Apresentando dados do banco de dados no template

--- core/views.py
+++ core/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Produto
+
+
 def index(request):
+    produtos = Produto.objects.all()
+
     context = {
-        'curso': 'Programação web com django framework',
-        'outro': 'Django é massa'
+        'curso': 'Programação Web com Django Framework',
+        'outro': 'Django é massa!',
+        'produtos': produtos
     }
     return render(request, 'index.html', context)
 
+
 def contato(request):
     return render(request, 'contato.html')
+
+
+def produto(request, pk):
+    prod = Produto.objects.get(id=pk)
+    context = {
+        'produto': prod
+    }
+    return render(request, 'produto.html', context)
+
+
+
